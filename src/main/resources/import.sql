ab2e3e60f9ec27347d6eca8dfe8d6043fadcf0ae
shino09 <bambasten9@gmail.com>
2020-12-07 22:23:12 -0300

    SUBIDA JAVA SPRING JPA

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (1,'juan','duran','jduran@pad.cl','2020-12-07');
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (2,'cristian','echenique','cechenique@pad.cl','2020-12-25');
